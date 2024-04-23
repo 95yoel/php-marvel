63ef94e7606a6077cdb02a4ed1d3bc7ceb242ded
Yoel Villa <villayoel@gmail.com>
2024-04-23 17:53:34 +0200

    update css

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
     $data = json_decode($result, true);
     // Close cURL
     curl_close($curl_handle);
-    echo $result
 ?>
 
-
+<head>
+    <meta charset="UTF-8"/>
+    <title>Next marvel film</title>
+    <meta name="description" content="Next marvel film"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
 
 <main>
-    <h2>Next marvel film</h2>
+    <section>
+        <img src="<?= $data["poster_url"];?>" width="300" alt="Logo" style="border-radius:10px;"/>
+    </section>
+    <hgroup>
+        <h2><?= $data["title"];?>is released in <?= $data["days_until"];?> days</h2>
+        <p>Release date <?= $data["release_date"];?></p>
+        <p>The next marvel film is : <?= $data["following_production"]["title"];?> <small><?= $data["following_production"]["release_date"];?></small></p>
+    </hgroup>
 </main>
-
-<style>
-
-    body{
-        display: grid;
-        place-content: center;
-    }
-
-</style>
